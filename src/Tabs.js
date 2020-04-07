8747d1843bc88db75c77315a0cf9a57da1b33c14
Martyna <martyna.gozda@gmail.com>
2020-04-07 13:31:41 +0200

    styleTheTabsMenu

--- src/Tabs.js
+++ src/Tabs.js
@@ -1,5 +1,6 @@
 import React, { useState } from "react";
 import "./Tab.scss";
+import one from "./img/icons/one.png";
 
 const Tabs = props => {
   const [activeTabIndex, setActiveTabIndex] = useState(0);
@@ -36,7 +37,9 @@ const Tabs = props => {
 export default function TabsMenu() {
   return (
     <div className="TabsMenu">
-      <h2>Step by step</h2>
+      <h2>Jak to działa?</h2>
+      <h4>Krok po kroku</h4>
+      <h3><img src={one} alt={one} />Wybierz jedną z opcji rezerwacji na jachcie</h3>
       <Tabs>
         <div title="Jacht">
         blbalbalbalb
