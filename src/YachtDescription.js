24e0f9c8e01707f73554da7a37373783f50bbc3c
Martyna <martyna.gozda@gmail.com>
2020-04-08 17:28:55 +0200

    newComponentYachtDescription

---
+++ src/YachtDescription.js
@@ -0,0 +1,36 @@
+import React, { Component } from "react";
+import "./Tab.scss";
+import "./Yachts.scss";
+
+
+class YachtDescription extends Component {
+
+    render () {
+
+        return (
+
+         <div className="class-box">
+         <div>
+         <div className="image-yacht"><img src={(this.props.textComponent.image)} alt={("this.props.firstGap.name")} /></div>
+         <div className="text-component">
+         <h3>{this.props.textComponent.name}</h3>
+         <p>{this.props.textComponent.price}/osoba</p>
+         <h4>Liczba miejsc</h4>
+         <p>{this.props.textComponent.person}</p>
+         <h4>Rocznik jachtu</h4>
+         <p>{this.props.textComponent.year}</p>
+         <h4>Udogodnienia</h4>
+         <ul>
+         <li>{this.props.textComponent.amenities[0]}</li>
+         <li>{this.props.textComponent.amenities[1]}</li>
+         <li>{this.props.textComponent.amenities[2]}</li>
+         </ul>
+         </div>
+         </div>
+         </div>)
+
+    }
+};
+
+
+export default YachtDescription;
--- src/Yachts.js
+++ src/Yachts.js
@@ -1,13 +1,15 @@
 import React, { useState } from "react";
-import "./Tabs.js"
+import "./Tabs.js";
 import "./Tab.scss";
-import "./Yachts.scss"
+import "./Yachts.scss";
+import YachtDescription from "./YachtDescription.js";
 import one from "./img/icons/one.png";
 import two from "./img/icons/two.png";
-import three from "./img/icons/three.png";
-import yacht from "./img/icons/boat-2.png";
+
+
 
 const Tabs = props => {
+
   const [activeTabIndex, setActiveTabIndex] = useState(0);
   const activeTab = props.children[activeTabIndex];
   return (
@@ -25,7 +27,7 @@ const Tabs = props => {
           </button>
         ))}
       </div>
-      <div className="tab-indicator-container">
+      <div id="yacht-tab-indicator-container" className="tab-indicator-container">
         <div
           className="tab-indicator"
            style={{
@@ -39,27 +41,85 @@ const Tabs = props => {
   );
 };
 
-export default function YachtClass() {
+
+export default function YachtClass(props) {
+  const comfort = {
+    image: require("./img/comfort.jpg"),
+    name: "Comfort",
+    price: 1590,
+    person: "8 max 10",
+    year: "2010 lub nowszy",
+    amenities: ["3 lub 4 zamykane kabiny", "rozkładana kanapa dwuosobowa w salonie", "2 łazienki"]
+  };
+
+  const premier = {
+    image: require("./img/premier.jpg"),
+    name: "Premier",
+    price: 1790,
+    person: "8 max 10",
+    year: "2014 lub nowszy",
+    amenities: ["3 lub 4 zamykane kabiny", "rozkładana kanapa dwuosobowa w salonie", "2 łazienki"]
+  };
+
+  const premierPlus = {
+    image: require("./img/premier-plus.jpg"),
+    name: "Premier +",
+    price: 2090,
+    person: "10 max 11",
+    year: "2014 lub nowszy",
+    amenities: ["5 zamykanych kabiny", "nie kwaterujemy nikogo w salonie!", "2 łazienki"]
+  };
+
+  const katComfort = {
+    image: require("./img/kat-comfort.jpg"),
+    name: "Comfort",
+    price: "2090 PLN",
+    person: "12",
+    year: "2014 lub nowszy",
+    amenities: ["4 zamykane kabiny", "2 miejsca do spania w salonie", "4 łazienki"]
+  };
+
+  const katPremier = {
+    image: require("./img/kat-premier.jpg"),
+    name: "Premier",
+    price: "2290 PLN",
+    person: "12",
+    year: "2014 lub nowszy",
+    amenities: ["4 zamykane kabiny", "rozkładana kanapa dwuosobowa w salonie", "4 łazienki", "większa powierzchnia w porównaniu do katamaranów klasy Comfort"]
+  };
+
+  // const katPremierPlus = {
+  //   image: require("./img/kat-premier-plus.jpg"),
+  //   name: "Premier +",
+  //   price: 2090,
+  //   person: "10 max 11",
+  //   year: "2014 lub nowszy",
+  //   amenities: ["5 zamykanych kabiny", "nie kwaterujemy nikogo w salonie!", "2 łazienki"]
+  // };
+
   return (
-    <div className="TabsMenu">
+
+    <div id="YachtMenu" className="TabsMenu">
     <h2>Jak to działa?</h2>
     <h4>Krok po kroku</h4>
       <h3><img src={one} alt={one} />Wybierz jednostkę a następnie klasę jachtu</h3>
       <Tabs>
       <div title="Jacht">
       <div className="class-box">
-      <div className="comfort"></div>
-      <div className="premier"></div>
-      <div className="premier+"></div>
+      <YachtDescription textComponent={comfort}/>
+      <YachtDescription textComponent={premier}/>
+      <YachtDescription textComponent={premierPlus}/>
+
       </div>
       </div>
-        <div title="Katamaran">
-        <div className="class-box">
-        <div className="comfort"></div>
-        <div className="premier"></div>
-        <div className="premier+"></div>
-        </div>
-        </div>
+      <div title="Katamaran">
+      <div className="class-box" style={{justifyContent: "space-evenly"}}>
+      <YachtDescription textComponent={katComfort}/>
+      <YachtDescription textComponent={katPremier}/>
+
+
+      </div>
+      </div>
       </Tabs>
     </div>
   );
