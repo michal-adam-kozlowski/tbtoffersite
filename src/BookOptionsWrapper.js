a839244362f5923b4dc421532328d3688565fa97
Martyna <martyna.gozda@gmail.com>
2020-04-23 12:58:18 +0200

    updateNavBar

--- src/BookOptionsWrapper.js
+++ src/BookOptionsWrapper.js
@@ -6,21 +6,21 @@ const wholeYacht = {
   title: "Jacht",
   name: "Cały jacht (7-12 osób)",
   icon: "fas fa-ship fa-7x",
-  info: "Jeżeli chcesz zapisać się na rejs z całą swoją ekipią, skorzystaj z opcji <span style={{fontWeight: 600}}>rezerwuj cały jacht</span>. Nie musicie mieć pojęcia o żeglowaniu - przydzielimy Wam skippera, który wszystkim się zajmie. I w sumie to najlepsze co możecie zrobić! Dlaczego? Bo jedziecie całą paczką i oszczędzacie do 120 PLN/os. w stosunku do zapisów indywidualnych."
+  info: "Jeżeli chcesz zapisać się na rejs z całą swoją ekipią, skorzystaj z opcji rezerwuj cały jacht. Nie musicie mieć pojęcia o żeglowaniu - przydzielimy Wam skippera, który wszystkim się zajmie. I w sumie to najlepsze co możecie zrobić! Dlaczego? Bo jedziecie całą paczką i oszczędzacie do 120 PLN/os. w stosunku do zapisów indywidualnych."
 };
 
 const cabin = {
   title: "Kabina",
   name: "Kabina (2 osoby)",
   icon: "fas fa-user-friends fa-7x",
-  info: "Jeżeli jest was dwoje i zależy Wam na gwarancji zakwaterowania w zamykanej kabinie z podwójnym łóżkiem, skorzystaj z opcji rezerwuj miejsce na jachcie lub kabinę</span>, wybierz Klasę Jachtu, kliknij Rezerwuj i następnie zaznacz - Kabina 2- osobowa. W kolejnym kroku podaj swoje dane i dane osoby towarzyszącej."
+  info: "Jeżeli jest was dwoje i zależy Wam na gwarancji zakwaterowania w zamykanej kabinie z podwójnym łóżkiem, skorzystaj z opcji rezerwuj miejsce na jachcie lub kabinę wybierz Klasę Jachtu, kliknij Rezerwuj i następnie zaznacz 'Kabina 2-osobowa'. W kolejnym kroku podaj swoje dane i dane osoby towarzyszącej."
 };
 
 const solo = {
   title: "Solo",
   name: "Pojedyncze miejsce (1 osoba)",
   icon: "fas fa-user-alt fa-7x",
-  info: "Jeżeli chcesz zapisać na rejs tylko siebie (albo siebie i kilku znajomych) skorzystaj z opcji <span style={{fontWeight: 600}}>rezerwuj miejsce na jachcie lub kabinę</span>, wybierz Klasę Jachtu, kliknij Rezerwuj i następnie zaznacz ile osób chcesz zgłosić. W kolejnym kroku podaj dane swoje i wszystkich zgłaszanych przez Ciebie osób i już! Witamy na pokładzie!"
+  info: "Jeżeli chcesz zapisać na rejs tylko siebie (albo siebie i kilku znajomych) skorzystaj z opcji rezerwuj miejsce na jachcie lub kabinę, wybierz Klasę Jachtu, kliknij Rezerwuj i następnie zaznacz ile osób chcesz zgłosić. W kolejnym kroku podaj dane swoje i wszystkich zgłaszanych przez Ciebie osób i już! Witamy na pokładzie!"
 }
 
 const Tabs = props => {
